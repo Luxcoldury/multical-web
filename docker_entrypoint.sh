8a5d6549895769334c3cdaceb6891a1b10494b29
Luxcoldury <luxcoldury@gmail.com>
2023-06-07 20:15:54 +0800

    bugfix

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$1" = 'multical-web']; then
+if [ "$1" = 'multical-web' ]; then
     cd /multical-web
     source /multical-web-env/bin/activate
     flask run -h 0.0.0.0 -p 5000
